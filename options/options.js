54d5e46f73c60de3822830eba9b96826da932155
Crowdbotics <team@crowdbotics.com>
2023-03-23 03:59:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-sun-39517",
+  url: "https://delicate-sun-39517.botics.co",
+  api: "https://delicate-sun-39517.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
